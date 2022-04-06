4d80037f8a3b8834a7f44f4ca9eaf7ab450bf955
razvan6518 <razvan.raindl2@gmail.com>
2022-04-06 11:11:05 +0300

    Add initial files

---
+++ server/src/test/java/com/razvan/yourownchef/YourownchefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razvan.yourownchef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourownchefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
